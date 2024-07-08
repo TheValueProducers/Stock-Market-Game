d229fe28f9c7dc1955b8b4d2040cea0b2760ec64
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2024-07-08 18:51:42 +0700

    Restructured the migration files

--- back-end/config/database.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config()
-const {DB_USER, DB_PASSWORD, DB_DATABASE, DB_HOST} = process.env;
-const { Sequelize } = require('sequelize');
-
-// Option 1: Passing a connection URI
-
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:5432/${DB_DATABASE}`) // Example for postgres
-
-module.exports = sequelize;
