f7219c0ed227657566fde087a0a35dddd5f53e47
TheValueProducers <145182357+TheValueProducers@users.noreply.github.com>
2024-07-12 18:54:36 +0700

    Successfully tested the game route and the player route

--- back-end/routes/userRoute.js
+++ back-end/routes/userRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
-const { authenticateUser } = require("../middleware/authenticate")
-const {loginEndpoint, registerUser} = require("../controllers/userController")
+
+const {loginEndpoint, registerUser, authenticateUser, userLogout } = require("../controllers/userController")
 
 router.post("/login", authenticateUser(passport), loginEndpoint)
 router.post("/register", registerUser)
+router.post("/logout", userLogout )
 
 
 module.exports = router;
